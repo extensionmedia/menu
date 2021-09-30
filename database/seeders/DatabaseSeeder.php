e1cb38c43ccf769ddfeacb1e6216aa2a7518b6b4
yassine_desktop <elmeftouhi@extensionmedia.ma>
2021-09-30 22:12:41 +0100

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        $this->call(ProduitCategoryTableSeeder::class);
+        $this->call(ProduitMarqueTableSeeder::class);
+        $this->call(ProduitStatusTableSeeder::class);
+        $this->call(ProduitColorTableSeeder::class);
+        $this->call(ProduitTypeTableSeeder::class);
+        $this->call(MagasinTableSeeder::class);
+        $this->call(ProduitTableSeeder::class);
+        $this->call(ProduitOfMagasinTableSeeder::class);
+        $this->call(FournisseurTableSeeder::class);
+    }
+}
---
+++ resources/views/container/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600&display=swap" rel="stylesheet">
+    @yield('includes')
+
+    <title>{{config('app.name')}}</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <script src="{{asset('js/app.js')}}"></script>
+</head>
+<body>
+    <div class="content w-screen h-screen bg-no-repeat overflow-auto" style="background-image: url({{asset('img/background_04.jpg')}})">
+        <div class="w-full xl:w-10/12 xl:mx-auto pb-8">
+            @include('container.common.top')
+            <div class="px-4 xl:px-0">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+</body>
+</html>
