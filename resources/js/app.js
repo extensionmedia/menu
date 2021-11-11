daf048268819bf700bdb4b62f440ca662c11fb0d
yassine_desktop <elmeftouhi@extensionmedia.ma>
2021-11-11 23:16:18 +0100

    swall

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 require('./bootstrap');
 
 import Alpine from 'alpinejs';
+import Swal from 'sweetalert2'
 
+var moment = require('moment');
+
+window.Swal = Swal;
 window.Alpine = Alpine;
 
 Alpine.start();
